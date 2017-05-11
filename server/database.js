8353b2a3373adf876ab98a717fb7103801ab921c
Charles Kim <cjkim0119@gmail.com>
2017-05-11 14:56:40 -0700

    (test) set up Heroku DB

--- server/database.js
+++ server/database.js
@@ -4,7 +4,9 @@ var request = require('request');
 var Sequelize = require('sequelize');
 var flash = require('express-flash');
 
-var sequelize = new Sequelize('postgres://localhost:5432/test');
+var databaseUrl = PROCESS.ENV.DATABASE_URL || 'postgres://localhost:5432/test';
+var sequelize = new Sequelize(databaseUrl);
+
 sequelize
   .authenticate()
   .then(function(err) {
@@ -23,8 +25,48 @@ var User = sequelize.define('users', {
   }
 });
 
+var Recipe = sequelize.define('recipes', {
+  username: {
+    type: Sequelize.STRING
+  },
+  recipeId: {
+    type: Sequelize.INTEGER
+  },
+  title: {
+    type: Sequelize.STRING
+  },
+  image: {
+    type: Sequelize.STRING
+  },
+  usedIngredientCount: {
+    type: Sequelize.INTEGER
+  },
+  missedIngredientCount: {
+    type: Sequelize.INTEGER
+  }
+});
+
 sequelize.sync();
 
+var createRecipe = (req, res) => {
+  Recipe.create({
+    username: req.body.username,
+    recipeId: req.body.recipeId,
+    title: req.body.title,
+    image: req.body.image,
+    usedIngredientCount: req.body.usedIngredientCount,
+    missedIngredientCount: req.body.missedIngredientCount
+  });
+
+  res.send();
+};
+
+var retrieveFavorites = (req, res) => {
+  Recipe.findAll({where: {username: req.body.username}})
+    .then(function(recipes) {
+      res.send(recipes);
+    });
+};
 
 var createUser = (req, res) => {
   User.findOne({where: {username: req.body.user}}).then(function(user) {
@@ -64,3 +106,5 @@ var checkIfUserExists = (req, res) => {
 
 module.exports.checkIfUserExists = checkIfUserExists;
 module.exports.createUser = createUser;
+module.exports.createRecipe = createRecipe;
+module.exports.retrieveFavorites = retrieveFavorites;
